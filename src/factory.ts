06dd8ca7dc4da66163244265a82df6f181c07363
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-07-14 14:04:21 +0100

    canvas command N and L

---
+++ src/factory.ts
@@ -0,0 +1,44 @@
+import * as createCanvas from './create-canvas';
+import * as drawLine from './draw-line';
+
+let canvas: string;
+let canvasArr: (string | number)[] = [];
+let width: number;
+
+const converToString = (arr: (string | number)[], index: number) => {
+    let checkCanvas = arr.join('');
+    for (let i = 0; i < 6; i++) {
+        checkCanvas = [checkCanvas.slice(0, index), '\n', checkCanvas.slice(index)].join('');
+        index += 23;
+    }
+    const regex = new RegExp('0', 'g');
+    const stringCanvas = checkCanvas.replace(regex, ' ');
+    return stringCanvas;
+}
+
+export const runCommands = (values: string) => {
+    let canvasValues = values.split(' ');
+    try {
+        if (canvasValues[0] === 'N') {
+            const w = Number(canvasValues[1]);
+            const h = Number(canvasValues[2]);
+            width = Number(canvasValues[1]) + 2;
+            canvasArr = createCanvas.drawCanvas(w, h);
+            canvas = converToString(canvasArr, width);
+            console.log(canvas);
+            return;
+        } else if (canvasValues[0] === 'L' && canvasArr) {
+            const x1 = Number(canvasValues[1]);
+            const y1 = Number(canvasValues[2]);
+            const x2 = Number(canvasValues[3]);
+            const y2 = Number(canvasValues[4]);
+
+            canvasArr = drawLine.line(x1, y1, x2, y2, canvasArr, width);
+            canvas = converToString(canvasArr, width);
+            console.log(canvas);
+            return;
+        }
+    } catch(err) {
+        throw new Error('Unable to draw canvas!');
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,20 @@
-import * as createCanvas from './create-canvas';
+import * as factory from './factory';
+import readline from 'readline';
 
-let canvas;
-let canvasArr;
-let width;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-const converToString = (arr: (string | number)[], index: number) => {
-    let checkCanvas = arr.join('');
-    for (let i = 0; i < 6; i++) {
-        checkCanvas = [checkCanvas.slice(0, index), '\n', checkCanvas.slice(index)].join('');
-        index += 23;
-    }
-    const regex = new RegExp('0', 'g');
-    const stringCanvas = checkCanvas.replace(regex, ' ');
-    return stringCanvas;
+const command = () => {
+    rl.question("Enter command: ", function(values) {
+        if (values === 'Q') {
+            rl.close();
+        } else {
+            factory.runCommands(values);
+            command();
+        }
+    });
 }
 
-const runCommands = (values: string) => {
-    let canvasValues = values.split(' ');
-    try {
-        if (canvasValues[0] === 'N') {
-            const w = Number(canvasValues[1]);
-            const h = Number(canvasValues[2]);
-            width = Number(canvasValues[1]) + 2;
-            canvasArr = createCanvas.drawCanvas(w, h);
-            canvas = converToString(canvasArr, width);
-            console.log(canvas);
-            return;
-        } 
-    } catch(err) {
-        throw new Error('Unable to draw canvas!');
-    }
-}
-
-runCommands('N 5 5');
+command();
